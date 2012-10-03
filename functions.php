4d5732a2d3f01e32328538d842f4b4897e955918
DJ Mosca <djmosc@gmail.com>
2012-10-03 02:07:34 +0100

    Added insert into array function

--- functions.php
+++ functions.php
@@ -227,3 +227,32 @@ function get_editer_option($option){
 	$options = get_option('editer_theme_options');
 	return $options[$option];
 }
+
+if ( ! function_exists( 'array_insert' )) {
+	function array_insert(&$array,$element,$position=null) {
+		if (count($array) == 0) {
+			$array[] = $element;
+		} elseif (is_numeric($position) && $position < 0) {
+			if((count($array)+position) < 0) {
+				$array = array_insert($array,$element,0);
+			} else {
+				$array[count($array)+$position] = $element;
+			}
+		} else if (is_numeric($position) && isset($array[$position])) {
+			$part1 = array_slice($array,0,$position,true);
+			$part2 = array_slice($array,$position,null,true);
+			$array = array_merge($part1,array($position=>$element),$part2);
+			foreach($array as $key=>$item) {
+				if (is_null($item)) {
+					unset($array[$key]);
+				}
+			}
+		} else if (is_null($position)) {
+			$array[] = $element;
+		} else if (!isset($array[$position])) {
+			$array[$position] = $element;
+		}
+		$array = array_merge($array);
+		return $array;
+	}
+}
